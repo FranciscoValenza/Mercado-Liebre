c32432fb8ef9af7eb11532c54e7527166e3cbf79
FranciscoValenza <franciscovalenza@gmail.com>
2022-09-02 22:39:56 -0300

    proyecto casi terminado

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const path = require('path')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.use(express.static(__dirname +'/views'));
+
+app.get('/', (req, res) => {
+    res.send('ahora si entre')
+})
+
+app.get('/mostrar_sitio', (req, res) => {
+    res.sendFile((__dirname + '/views/home.html'))
+})
+
+app.get('/register.html', (req,res) => {
+    res.sendFile(path.resolve(__dirname + '/views/register.html'));
+});
+
+app.get('/login.html', (req,res) => {
+    res.sendFile(path.resolve(__dirname + '/views/login.html'));
+});
+
+app.get('*', (req, res) => {
+    res.send('ruta restringida ****')
+})
+
+app.listen(3100, function() {
+    console.log("Levantando un servidor con Express")
+})
